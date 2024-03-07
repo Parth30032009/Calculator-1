8753030b54dd5f3ab324af982599b101d3a72aa1
Parth30032009 <ps802sharma@gmail.com>
2024-03-07 18:45:15 +0530

    adding files

---
+++ cal.js
@@ -0,0 +1,26 @@
+function add(){
+    let num1=parseFloat(document.getElementById("num1").value);
+    let num2=parseFloat(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML=num1+"+"+num2+"="+(num1+num2);
+}
+function subtract(){
+    let num1=parseFloat(document.getElementById("num1").value);
+    let num2=parseFloat(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML=num1+"-"+num2+"="+(num1-num2);
+    
+}
+function multiply(){
+    let num1=parseFloat(document.getElementById("num1").value);
+    let num2=parseFloat(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML=num1+"*"+num2+"="+(num1*num2);
+    
+}
+
+function divide(){
+    let num1=parseFloat(document.getElementById("num1").value);
+    let num2=parseFloat(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML=num1+"/"+num2+"="+(num1/num2);
+    
+}
+
+
